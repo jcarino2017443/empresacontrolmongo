022eda0d5cd5a59d9b794037563220a4f3706d03
unknown <jcarino-2017443@kinal.edu.gt>
2021-09-25 08:12:25 +0100

    conexion

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ var app = require('./app')
 var puerto = 3800;
 
 moongose.Promise = global.Promise;
-moongose.connect('mongodb://localhost:27017/Curso', {useNewUrlParser: true, useUnifiedTopology:true}).then(()=>{
-    console.log('La conexion se ha hecho exitosamente')
-    app.listen(puerto, ()=>{
+moongose.connect('mongodb+srv://jeffrey:divertido19@basededatos.bgvve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology:true}).then(()=>{
+    console.log('La conexion se ha hecho exitosamente');
+
+    app.listen(process.env.PORT || puerto, ()=>{
         console.log(`el servidor esta corriendo en el puerto ${puerto}`)
     })
 }).catch(function (err) {
     console.log(err)
 })
+//mongodb://localhost:27017/Curso
